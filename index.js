b3315f189518ca493b8bc32f42c6be6b82b0da1e
alma-iceee <alma.iceee@gmail.com>
2021-03-15 02:23:39 +0600

    Ping Pong! (Hello World!)

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (message.content === '!ping') {
+		// send back "Pong." to the channel the message was sent in
+		message.channel.send('Pong');
+	}
+});
+
+client.login(process.env.DISCORD_TOKEN);
