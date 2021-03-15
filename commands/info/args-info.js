02b83cd4a1bfc07ba6b5f5152d7906eab5e7be21
alma-iceee <alma.iceee@gmail.com>
2021-03-15 17:28:08 +0600

    Testing command with arguments

--- commands/info/args-info.js
+++ commands/info/args-info.js
@@ -2,13 +2,13 @@
 module.exports = {
 	name: 'args-info',
 	description: 'Arguments Info',
+	args: true,
+	usage: '<user> <role>',
 	execute(message, args) {
-		if (!args.length) {
-			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-		} else if (args[0] === 'foo') {
+		if (args[0] === 'foo') {
 			return message.channel.send('bar');
 		}
 
-		message.channel.send(`Arguments: ${args}`);
+		message.channel.send(`Arguments: ${args}\nArguments length: ${args.length}`);
 	},
 };
--- index.js
+++ index.js
@@ -38,6 +38,16 @@ client.on('message', message => {
 
 	const command = client.commands.get(commandName);
 
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${process.env.PREXIF}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
+
 	try {
 		command.execute(message, args);
 	} catch (err) {
