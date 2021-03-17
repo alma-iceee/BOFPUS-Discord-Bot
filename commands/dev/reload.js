6fafb6eec05adf308c52043a04172572a0661517
alma-iceee <alma.iceee@gmail.com>
2021-03-17 10:55:15 +0600

    Syntax fix in reload.js

--- commands/dev/reload.js
+++ commands/dev/reload.js
@@ -5,7 +5,10 @@ module.exports = {
 	name: 'reload',
 	description: 'Reloads a command',
 	execute(message, args) {
-		if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
+		if (!args.length) {
+			return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
+		}
+
 		const commandName = args[0].toLowerCase();
 		const command = message.client.commands.get(commandName)
 			|| message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
